7bbd5940e8ff74c6e1c3bd93a284dfe45c3c97f3
Elijah Hoffman <elijah.hoffman@nwcc.nsw.edu.au>
2026-08-26 00:08:41 +1000

    Update LED constructor to use a brightness constant instead of parameter

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from mini_joystick_i2c import MiniJoyStickI2C
 MATRIX_PIN = Pin(26, Pin.OUT)
 PIXELS_X = 16
 PIXELS_Y = 16
+LED_BRIGHTNESS = 50
+MINE_COLOUR = (255, 0, 0)
 
 class PanelManager:
     """Panel Manager class -- controls / manages all LED objects and interaction with the neopixel matrix"""
@@ -15,7 +17,7 @@ class PanelManager:
         """Initialises panel manager object with the LED pixels and matrix as attributes."""
         self._led_matrix = neopixel.Neopixel(matrix_pin, pixels_x * pixels_y)
         # Generic values used here for LED objects, list will ultimately be generated in setup method using appropriate LED subclass
-        self._led_list = list(LED((x, y), (255, 255, 255), 20) for y in range(pixels_y) for x in range(pixels_x))
+        self._led_list = list(LED((x, y), (255, 255, 255)) for y in range(pixels_y) for x in range(pixels_x))
         # Creates a 2 dimensional array using grid coordinates as the indexes to retrieve the index appropriate for LED matrix
         # that is in a serpantine array format. This is done by counting up if the row index is odd and otherwise counting down
         self._index_converter = [[x for x in (range(pixels_x * y, pixels_x * (y + 1), 1) if y % 2 != 0 else range(pixels_x * (y + 1) - 1, pixels_x * y - 1, -1))] for y in range(pixels_y)]
@@ -41,11 +43,11 @@ class PanelManager:
 
 class LED:
     """LED class -- used for each pixel in the matrix"""
-    def __init__(self, grid_pos: tuple[int, int], colour: tuple[int, int, int], brightness: int):
+    def __init__(self, grid_pos: tuple[int, int], colour: tuple[int, int, int]):
         """Instantiates an LED pixel object with the appropriate attributes."""
         self._grid_pos = grid_pos
         self._colour = colour
-        self._brightness = brightness
+        self._brightness = LED_BRIGHTNESS
         self._is_on = False
 
     def set_colour(self, colour: tuple[int, int, int]):
@@ -71,4 +73,6 @@ class LED:
 
     def draw(self, panel_manager: PanelManager):
         """Public method called to update the LED pixel colour in the panel matrix using message passing."""
-        panel_manager.update_pixel(self._grid_pos, self._calculate_rgb())
+        if self._is_on:
+            panel_manager.update_pixel(self._grid_pos, self._calculate_rgb())
+
